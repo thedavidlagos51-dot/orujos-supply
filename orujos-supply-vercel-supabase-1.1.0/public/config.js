78dba5a093e97cb0a69799f5093c70509244f156
thedavidlagos51-dot <thedavidlagos51@gmail.com>
2026-09-12 23:24:42 -0600

    config.js

--- orujos-supply-vercel-supabase-1.1.0/public/config.js
+++ orujos-supply-vercel-supabase-1.1.0/public/config.js
@@ -1,4 +1,2 @@
-window.ORUJOS_CONFIG = {
-  supabaseUrl: 'https://cagapiwgonhwkzfguwzh.supabase.co',
-  supabasePublishableKey: 'sb_publishable_eE9f8FKmB2YbsCI6ws5t9w_ujUIwl_g'
-};
+window.SUPABASE_URL = "https://gfxrsyjsgsksqbuybcor.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_eE9f8FKmB2YbsCI6ws5t9w_ujUIwl_g";
